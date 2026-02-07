ee1d70c3df8e6f352bb6d43e5eaf74670edaf59f
Admin <admin@192.168.2.27>
2026-02-07 21:50:39 +0700

    chore(deps): update dependencies and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      path: 'path-browserify',
+    },
+  },
   build: {
     chunkSizeWarningLimit: 20000, // 20MB - suppress warnings
   },
